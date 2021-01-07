db25d2fd61189b0e6d07e9a4db357ea0cb58122e
westdn <west.naoya@gmail.com>
2021-01-08 02:25:09 +0900

    Update keymap.c

--- dz60/keymaps/60_split_rshift/keymap.c
+++ dz60/keymaps/60_split_rshift/keymap.c
@@ -119,10 +119,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_JPFL] = LAYOUT_60_ansi_split_space_rshift(
 		JP_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
-		_______,          KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, _______, _______, KC_PSCR, G(A(KC_PSCR)), XXXXXXX, KC_UP, XXXXXXX, JP_ZKHK,
-		MO(_ADJUST),          KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______, _______, KC_HOME, KC_PGUP,  KC_LEFT, KC_RGHT, _______,
-		KC_LSFT,          _______, _______, _______,  _______, _______,  _______, _______, KC_END,  KC_PGDN,  KC_DOWN,          _______, _______,
-		_______, _______,          _______,      KC_SPC,           KC_SPC,        KC_SPC,         _______, _______,          _______, _______
+		_______,   KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, _______, _______, KC_PSCR, G(A(KC_PSCR)),  XXXXXXX,  KC_UP, XXXXXXX, JP_ZKHK,
+		MO(_ADJUST),  KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______, _______, KC_HOME,    KC_PGUP,    KC_LEFT,  KC_RGHT,   _______,
+		KC_LSFT,        _______, _______, _______,  _______, _______,  _______, _______, KC_END,   KC_PGDN,    KC_DOWN,  _______,   _______,
+		_______, _______,          _______,      KC_SPC,           KC_SPC,        KC_SPC,         _______, _______,         _______,  _______
 		),
 
 	[_ADJUST] = LAYOUT_60_ansi_split_space_rshift(
